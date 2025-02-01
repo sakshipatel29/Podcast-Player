d2196e422cab4a15df1bad1a3b8ef196869a301f
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-01 15:02:43 -0600

    Using GCC for user logins and added a block of code, to receive encoded token when user login.

--- podcast/src/App.js
+++ podcast/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  const handleCallBack = (res) => {
+    console.log("Encoded Token:" + res.credential);
+  }
+
+  useEffect(()=>{
+    /*global google */
+    google.accounts.id.initialize({
+      client_id: "698771483813-tqb3e6dj8erql1hmc8328m8ib705iv7b.apps.googleusercontent.com",
+      callback: handleCallBack
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("SignIn"),
+      { theme: 'outline', size: 'large'}
+    )
+},[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Podcast App</h1>
+      <div id="SignIn"></div>
     </div>
   );
 }
